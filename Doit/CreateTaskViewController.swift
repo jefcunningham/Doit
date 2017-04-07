09e49c6fdfef94e48372a4f97d3a8ba8d1b4ce31
Jef Cunningham <jef@snowball.is>
2017-04-07 00:22:45 -0700

    Updating from lecture 65

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Jef Cunningham on 4/6/17.
+//  Copyright © 2017 Jef Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        // create a task from the outlet information
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        
+        // add new task to the array in the other viewController
+        previousVC.tasks.append(task)
+        // you have to reload the tableview on the other viewController
+        previousVC.tableView.reloadData()
+        // animate back to the other viewController after pressing the button
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    
+
+}
