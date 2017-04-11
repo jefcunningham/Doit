1ab441157556f926ad46f844b3ca4be23fb8941c
Jef Cunningham <jef@snowball.is>
2017-04-10 18:02:24 -0700

    Changed important emoji

--- Doit/TasksViewController.swift
+++ Doit/TasksViewController.swift
@@ -38,7 +38,7 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
         let cell = UITableViewCell()
         let task = tasks[indexPath.row]
         if task.important {
-            cell.textLabel?.text = "⚠️\(task.name!)"
+            cell.textLabel?.text = "⭐️ \(task.name!)"
         } else {
             cell.textLabel?.text = task.name!
         }
